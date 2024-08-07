05d7ebf6cfc2540124cd334af95b66913ac476ae
Zihao Xu <xzhseh@gmail.com>
2024-08-07 01:20:39 -0700

    placeholder

--- iceberg/src/test/scala/org/apache/spark/sql/delta/uniform/UniversalFormatSuite.scala
+++ iceberg/src/test/scala/org/apache/spark/sql/delta/uniform/UniversalFormatSuite.scala
@@ -17,15 +17,113 @@
 package org.apache.spark.sql.delta.uniform
 
 import java.util.UUID
-
-import org.apache.spark.sql.delta.{UniversalFormatMiscSuiteBase, UniversalFormatSuiteBase}
 import org.apache.hadoop.fs.Path
-
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.catalyst.TableIdentifier
+import org.apache.spark.sql.delta.icebergShaded.IcebergTransactionUtils
+import org.apache.spark.sql.delta.{DeltaLog, DeltaUnsupportedOperationException, UniFormWithIcebergCompatV1SuiteBase, UniFormWithIcebergCompatV2SuiteBase, UniversalFormatMiscSuiteBase, UniversalFormatSuiteBase}
+import org.apache.spark.sql.{DataFrame, Row}
 
 class UniversalFormatSuite
     extends UniversalFormatMiscSuiteBase
     with WriteDeltaHMSReadIceberg {
+  override def withTempTableAndDir(f: (String, String) => Unit): Unit = {
+    val tableId = s"testTable${UUID.randomUUID()}".replace("-", "_")
+    withTempDir { dir =>
+      val tablePath = new Path(dir.toString, "table")
+
+      withTable(tableId) {
+        f(tableId, s"'$tablePath'")
+      }
+    }
+  }
+
+  override def executeSql(sqlStr: String): DataFrame = {
+    write(sqlStr)
+    // return a null reference here to fulfill
+    // override requirement
+    null
+  }
+
+  test("create new UniForm table without manually enabling IcebergCompat - fail") {
+    allReaderWriterVersions.filter { case (_, w) => w != 6 }.foreach { case (r, w) =>
+      withTempTableAndDir { case (id, loc) =>
+        val e = intercept[DeltaUnsupportedOperationException] {
+          executeSql(s"""
+                        |CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
+                        |  'delta.universalFormat.enabledFormats' = 'iceberg',
+                        |  'delta.minReaderVersion' = $r,
+                        |  'delta.minWriterVersion' = $w
+                        |)""".stripMargin)
+        }
+        assert(e.getErrorClass == "DELTA_UNIVERSAL_FORMAT_VIOLATION")
+
+        val e1 = intercept[DeltaUnsupportedOperationException] {
+          executeSql(s"""
+                        |CREATE TABLE $id USING DELTA LOCATION $loc TBLPROPERTIES (
+                        |  'delta.universalFormat.enabledFormats' = 'iceberg',
+                        |  'delta.minReaderVersion' = $r,
+                        |  'delta.minWriterVersion' = $w
+                        |) AS SELECT 1""".stripMargin)
+        }
+        assert(e1.getErrorClass == "DELTA_UNIVERSAL_FORMAT_VIOLATION")
+      }
+    }
+  }
+
+  test("enable UniForm on existing table but IcebergCompat isn't enabled - fail") {
+    allReaderWriterVersions.filter { case (_, w) => w != 6 }.foreach { case (r, w) =>
+      withTempTableAndDir { case (id, loc) =>
+        executeSql(s"""
+                      |CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
+                      |  'delta.minReaderVersion' = $r,
+                      |  'delta.minWriterVersion' = $w
+                      |)""".stripMargin)
+
+        val e = intercept[DeltaUnsupportedOperationException] {
+          executeSql(s"ALTER TABLE $id SET TBLPROPERTIES " +
+            s"('delta.universalFormat.enabledFormats' = 'iceberg')")
+        }
+        assert(e.getErrorClass === "DELTA_UNIVERSAL_FORMAT_VIOLATION")
+      }
+    }
+  }
+}
+
+class UniFormWithIcebergCompatV1Suite
+    extends UniFormWithIcebergCompatV1SuiteBase
+    with WriteDeltaHMSReadIceberg {
+  override protected val allReaderWriterVersions: Seq[(Int, Int)] = (1 to 3)
+    .flatMap { r => (1 to 7).filter(_ != 6).map(w => (r, w)) }
+    // can only be at minReaderVersion >= 3 if minWriterVersion is >= 7
+    .filterNot { case (r, w) => w < 7 && r >= 3 }
+
+  override def withTempTableAndDir(f: (String, String) => Unit): Unit = {
+    val tableId = s"testTable${UUID.randomUUID()}".replace("-", "_")
+    withTempDir { dir =>
+      val tablePath = new Path(dir.toString, "table")
+
+      withTable(tableId) {
+        f(tableId, s"'$tablePath'")
+      }
+    }
+  }
+
+  override def executeSql(sqlStr: String): DataFrame = {
+    write(sqlStr)
+    // return a null reference here to fulfill
+    // override requirement
+    null
+  }
+}
+
+class UniFormWithIcebergCompatV2Suite
+  extends UniFormWithIcebergCompatV2SuiteBase
+    with WriteDeltaHMSReadIceberg {
+  override protected val allReaderWriterVersions: Seq[(Int, Int)] = (1 to 3)
+    .flatMap { r => (1 to 7).filter(_ != 6).map(w => (r, w)) }
+    // can only be at minReaderVersion >= 3 if minWriterVersion is >= 7
+    .filterNot { case (r, w) => w < 7 && r >= 3 }
+
   override def withTempTableAndDir(f: (String, String) => Unit): Unit = {
     val tableId = s"testTable${UUID.randomUUID()}".replace("-", "_")
     withTempDir { dir =>
--- spark/src/test/scala/org/apache/spark/sql/delta/UniversalFormatSuiteBase.scala
+++ spark/src/test/scala/org/apache/spark/sql/delta/UniversalFormatSuiteBase.scala
@@ -19,13 +19,12 @@ package org.apache.spark.sql.delta
 import org.apache.spark.sql.delta.DeltaConfigs.{COLUMN_MAPPING_MODE, ICEBERG_COMPAT_V2_ENABLED}
 import org.apache.spark.sql.delta.DeltaOperations.ManualUpdate
 import org.apache.spark.sql.delta.actions.{AddFile, Metadata}
-import org.apache.spark.sql.delta.commands.ReorgTableCommand
 import org.apache.spark.sql.delta.sources.DeltaSQLConf.DELTA_UNIFORM_ICEBERG_SYNC_CONVERT_ENABLED
 import org.apache.spark.sql.delta.util.Utils.try_element_at
-
 import org.apache.spark.sql.{DataFrameWriter, QueryTest, Row}
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.parser.ParseException
+import org.apache.spark.sql.delta.commands.DeltaReorgTableCommand
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.test.SQLTestUtils
 
@@ -207,106 +206,6 @@ trait UniversalFormatSuiteBase extends QueryTest with IcebergCompatUtilsBase {
     }
   }
 
-  test("CTAS new UniForm (Iceberg) table without manually enabling column mapping") {
-    // These are the versions with column mapping enabled
-    allReaderWriterVersions.foreach { case (r, w) =>
-      withTempTableAndDir { case (from_id, from_loc) =>
-        withTempTableAndDir { case (to_id1, to_loc1) =>
-          withTempTableAndDir { case (to_id2, to_loc2) =>
-            executeSql(
-              s"""
-                 |CREATE TABLE $from_id (PID INT, PCODE INT) USING DELTA LOCATION $from_loc
-                 | TBLPROPERTIES (
-                 |  'delta.minReaderVersion' = $r,
-                 |  'delta.minWriterVersion' = $w
-                 |)""".stripMargin)
-            executeSql(
-              s"""
-                 |INSERT INTO TABLE $from_id (PID, PCODE)
-                 | VALUES (1,2),(2,3),(3,4),(4,5),(5,6),(6,7),(7,8)""".stripMargin)
-            executeSql(
-              s"""
-                 |CREATE TABLE $to_id1 USING DELTA LOCATION $to_loc1 TBLPROPERTIES (
-                 |  'delta.universalFormat.enabledFormats' = 'iceberg',
-                 |  'delta.enableIcebergCompatV$compatVersion' = 'true',
-                 |  'delta.minReaderVersion' = $r,
-                 |  'delta.minWriterVersion' = $w
-                 |) AS SELECT * FROM $from_id""".stripMargin)
-            executeSql(
-              s"""
-                 |CREATE TABLE $to_id2 USING DELTA LOCATION $to_loc2 TBLPROPERTIES (
-                 |  'delta.universalFormat.enabledFormats' = 'iceberg',
-                 |  'delta.columnMapping.mode' = 'name',
-                 |  'delta.enableIcebergCompatV$compatVersion' = 'true',
-                 |  'delta.minReaderVersion' = $r,
-                 |  'delta.minWriterVersion' = $w
-                 |) AS SELECT * FROM $from_id""".stripMargin)
-
-            val metaData1 = spark.sessionState.catalog.getTableMetadata(
-              TableIdentifier(to_id1))
-            assert(metaData1.dataSchema.size == 2)
-
-
-            val result1 = executeSql(s"SELECT * FROM $to_id1 ORDER BY PID")
-            checkAnswer(result1, 1 to 7 map (i => Row(i, i + 1)))
-
-            val metaData2 = spark.sessionState.catalog.getTableMetadata(
-              TableIdentifier(to_id2))
-            assert(metaData2.dataSchema.size == 2)
-
-            val result2 = executeSql(s"SELECT * FROM $to_id2 ORDER BY PID")
-            checkAnswer(result2, 1 to 7 map (i => Row(i, i + 1)))
-          }
-        }
-      }
-    }
-  }
-
-  test(s"df overwrite with uniform does not trigger synchronous conversion") {
-    withTempTableAndDir { case (id, _) =>
-      getDfWriter("id1", "append", enableUniform = false).saveAsTable(id)
-      // Any table properties specified as part of a dataframe overwrite are not
-      // written to delta and so this does not enable uniform
-      getDfWriter("id2", "overwrite").saveAsTable(id)
-      assert(spark.sessionState.catalog.getTableMetadata(
-        TableIdentifier(id)).deltaUniformIceberg.isEmpty)
-    }
-  }
-
-  test(s"df overwrite uniform enabled does not trigger synchronous conversion") {
-    withTempTableAndDir { case (id, _) =>
-      getDfWriter("id1", "append", enableUniform = false).saveAsTable(id)
-      withSQLConf(
-        confStr -> "iceberg",
-        compatConfStr -> "true"
-      ) {
-        // Any table properties specified as part of a dataframe overwrite are not
-        // written to delta and so this does not enable uniform
-        getDfWriter("id1", "overwrite", enableUniform = false).saveAsTable(id)
-        assert(spark.sessionState.catalog.getTableMetadata(
-          TableIdentifier(id)).deltaUniformIceberg.isEmpty)
-      }
-    }
-  }
-
-  test("describe Uniform table should include Iceberg info") {
-    withTempTableAndDir {
-      case (id, loc) =>
-        executeSql(
-          s"""
-             |CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
-             |  'delta.universalFormat.enabledFormats' = 'iceberg',
-             |  'delta.enableIcebergCompatV$compatVersion' = true
-             |)""".stripMargin)
-
-        val descOutputDelta = executeSql(s"DESC EXTENDED $id").collect()
-        val descOutputDeltaMap =
-          descOutputDelta.map(r => (r.getString(0), r.getString(1))).toMap
-        assert(descOutputDeltaMap.get("Metadata location").exists(_.nonEmpty))
-        assert(descOutputDeltaMap.get("Converted delta version").contains("0"))
-    }
-  }
-
   test("REORG TABLE for table from None to corresponding icebergCompat version") {
     withTempTableAndDir { case (id, loc) =>
       executeSql(s"""
@@ -390,27 +289,6 @@ trait UniversalFormatSuiteBase extends QueryTest with IcebergCompatUtilsBase {
       runReorgTableForUpgradeUniform(id, compatVersion)
       val snapshot2 = deltaLog.update()
       checkFileNotRewritten(snapshot1, snapshot2)
-    }
-  }
-
-  test("REORG TABLE: command does not support where clause") {
-    withTempTableAndDir { case (id, loc) =>
-      val anotherCompatVersion = getCompatVersionOtherThan(compatVersion)
-      executeSql(s"""
-             | CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
-             |  'delta.universalFormat.enabledFormats' = 'iceberg',
-             |  'delta.enableIcebergCompatV$anotherCompatVersion' = 'true'
-             |)""".stripMargin)
-      val e = intercept[ParseException] {
-        spark.sessionState.sqlParser.parsePlan(
-          s"""
-             | REORG TABLE $id
-             | WHERE ID > 0
-             | APPLY (UPGRADE UNIFORM (ICEBERGCOMPATVERSION = $compatVersion))
-         """.stripMargin).asInstanceOf[ReorgTableCommand]
-      }
-      assert(e.getErrorClass === "PARSE_SYNTAX_ERROR")
-      assert(e.getMessage.contains("Syntax error at or near 'UPGRADE'"))
     }
   }
 
@@ -522,38 +400,6 @@ trait UniFormWithIcebergCompatV2SuiteBase extends UniversalFormatSuiteBase with 
     })
   }
 
-  test("overwrite with same partition column should pass") {
-    withTempTableAndDir {
-      case (id, _) =>
-        val data = Seq((1, "aaa"))
-
-        val df = spark.createDataFrame(data).toDF("col1", "col2")
-
-        df.write
-          .format("delta")
-          .partitionBy("col1")
-          .option("delta.enableIcebergCompatV2", "true")
-          .option("delta.universalFormat.enabledFormats", "iceberg")
-          .saveAsTable(id)
-
-        df.write
-          .format("delta")
-          .partitionBy("col1")
-          .mode("overwrite")
-          .option("delta.enableIcebergCompatV2", "true")
-          .option("delta.universalFormat.enabledFormats", "iceberg")
-          .saveAsTable(id)
-
-        spark.sql(s"MSCK REPAIR TABLE $id SYNC METADATA")
-
-        val descOutputDelta = executeSql(s"DESC EXTENDED $id").collect()
-        val descOutputDeltaMap =
-          descOutputDelta.map(r => (r.getString(0), r.getString(1))).toMap
-        assert(descOutputDeltaMap.get("Metadata location").exists(_.contains("00002")))
-        assert(descOutputDeltaMap.get("Converted delta version").contains("1"))
-    }
-  }
-
   test(
     "REORG TABLE: new files would have ICEBERG_COMPAT_VERSION tag if enableIcebergCompat is on") {
     withTempTableAndDir { case (id, loc) =>
@@ -595,84 +441,6 @@ trait UniFormWithIcebergCompatV2SuiteBase extends UniversalFormatSuiteBase with 
       val finalSnapshot = deltaLog.update()
       assert(finalSnapshot.getProperties("delta.enableIcebergCompatV2") === "true")
       assertAddFileIcebergCompatVersion(finalSnapshot, icebergCompatVersion = 2, count = 2)
-    }
-  }
-
-  test("REORG TABLE: exception would be thrown if it is a archived table") {
-    withTempTableAndDir { case (id, loc) =>
-      executeSql(
-        s"""
-           | CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
-           |  'delta.columnMapping.mode' = 'name'
-           |)
-           | """.stripMargin)
-      executeSql(s"ALTER TABLE $id SET TBLPROPERTIES (delta.timeUntilArchived = '5 days')")
-      val e = intercept[DeltaUnsupportedOperationException] {
-        runReorgTableForUpgradeUniform(id, 2)
-      }
-      assert(e.getErrorClass === "DELTA_ICEBERG_COMPAT_VIOLATION.WRONG_REQUIRED_TABLE_PROPERTY")
-    }
-  }
-
-  test("MAX column ID is preserved correctly for table when metadata changes") {
-    withTempTableAndDir {
-      case (table_id, loc) =>
-        withSQLConf(DELTA_UNIFORM_ICEBERG_SYNC_CONVERT_ENABLED.key -> "true") {
-          executeSql(
-            s"""
-               | CREATE TABLE $table_id (col1 INT, col2 String, col3 MAP<INT, STRING>)
-               | USING DELTA LOCATION $loc TBLPROPERTIES (
-               |  'delta.enableIcebergCompatV2' = 'true',
-               |  'delta.columnMapping.mode' = 'name',
-               |  'delta.universalFormat.enabledFormats' = 'iceberg'
-               |)
-               | """.stripMargin)
-          val deltaLog = DeltaLog.forTable(spark, TableIdentifier(table_id))
-          // Verify the initial max column id of the table
-          val firstRoundMetadata = deltaLog.update().metadata
-          assertResult(5)(DeltaConfigs.COLUMN_MAPPING_MAX_ID.fromMetaData(firstRoundMetadata))
-
-          // Construct a new metadata with no max column id appearing in the configuration
-          // of proposed new metadata
-          val secondRoundMetadata = DeltaColumnMapping.assignColumnIdAndPhysicalName(
-            newMetadata = Metadata(
-              schemaString = firstRoundMetadata.schemaString,
-              configuration = Map(
-                ICEBERG_COMPAT_V2_ENABLED.key -> "true",
-                COLUMN_MAPPING_MODE.key -> "name",
-                DeltaConfigs.UNIVERSAL_FORMAT_ENABLED_FORMATS.key -> "iceberg"
-              )
-            ),
-            oldMetadata = firstRoundMetadata,
-            isChangingModeOnExistingTable = false,
-            isOverwritingSchema = true
-          )
-          assertResult(5)(DeltaConfigs.COLUMN_MAPPING_MAX_ID.fromMetaData(secondRoundMetadata))
-
-          // Commit the new metadata to the table and verify the max column id
-          deltaLog.withNewTransaction(None) { txn =>
-            txn.updateMetadata(
-              secondRoundMetadata
-            )
-            txn.commit(Nil, ManualUpdate)
-          }
-          assertResult(5)(
-            DeltaConfigs.COLUMN_MAPPING_MAX_ID.fromMetaData(deltaLog.update().metadata)
-          )
-
-          // Add a new column to the table
-          // Verify the max column id after the new column is added
-          executeSql(s"ALTER TABLE $table_id ADD COLUMN col4 INT")
-          assertResult(6)(
-            DeltaConfigs.COLUMN_MAPPING_MAX_ID.fromMetaData(deltaLog.update().metadata)
-          )
-
-          // Write to the table & we are not expecting iceberg conversion failures
-          // Verify write succeeds
-          executeSql(s"INSERT INTO $table_id VALUES (1, 'Alice', Map(1, 'a'), 2)")
-          val df = spark.read.format("delta").table(table_id)
-          checkAnswer(df, Row(1, "Alice", Map(1 -> "a"), 2) :: Nil)
-        }
     }
   }
 }
@@ -746,50 +514,6 @@ trait UniversalFormatMiscSuiteBase extends IcebergCompatUtilsBase {
           errMsg.contains("Must be a comma-separated list of formats from the list"),
           errMsg
         )
-      }
-    }
-  }
-
-  test("create new UniForm table without manually enabling IcebergCompat - fail") {
-    allReaderWriterVersions.foreach { case (r, w) =>
-      withTempTableAndDir { case (id, loc) =>
-        val e = intercept[DeltaUnsupportedOperationException] {
-          executeSql(s"""
-                 |CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
-                 |  'delta.universalFormat.enabledFormats' = 'iceberg',
-                 |  'delta.minReaderVersion' = $r,
-                 |  'delta.minWriterVersion' = $w
-                 |)""".stripMargin)
-        }
-        assert(e.getErrorClass == "DELTA_UNIVERSAL_FORMAT_VIOLATION")
-
-        val e1 = intercept[DeltaUnsupportedOperationException] {
-          executeSql(s"""
-                 |CREATE TABLE $id USING DELTA LOCATION $loc TBLPROPERTIES (
-                 |  'delta.universalFormat.enabledFormats' = 'iceberg',
-                 |  'delta.minReaderVersion' = $r,
-                 |  'delta.minWriterVersion' = $w
-                 |) AS SELECT 1""".stripMargin)
-        }
-        assert(e1.getErrorClass == "DELTA_UNIVERSAL_FORMAT_VIOLATION")
-      }
-    }
-  }
-
-  test("enable UniForm on existing table but IcebergCompat isn't enabled - fail") {
-    allReaderWriterVersions.foreach { case (r, w) =>
-      withTempTableAndDir { case (id, loc) =>
-        executeSql(s"""
-               |CREATE TABLE $id (ID INT) USING DELTA LOCATION $loc TBLPROPERTIES (
-               |  'delta.minReaderVersion' = $r,
-               |  'delta.minWriterVersion' = $w
-               |)""".stripMargin)
-
-        val e = intercept[DeltaUnsupportedOperationException] {
-          executeSql(s"ALTER TABLE $id SET TBLPROPERTIES " +
-            s"('delta.universalFormat.enabledFormats' = 'iceberg')")
-        }
-        assert(e.getErrorClass === "DELTA_UNIVERSAL_FORMAT_VIOLATION")
       }
     }
   }
